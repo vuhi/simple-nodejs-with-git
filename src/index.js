a9657ecbeafda6d24cab4300e47549b0bd98f49a
vuhi <vuhi@mail.uc.edu>
2021-01-15 16:16:05 -0500

    add axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-  res.json('hello world');
+app.get('/', async (req, res) => {
+  
+  try {
+    const result = await axios.get('http://169.254.169.254/latest/meta-data');
+    res.json(result);
+  } catch(e) {
+    res.json(JSON.stringify(e));
+  }
 });
 
 const server = app.listen(port, () => {
