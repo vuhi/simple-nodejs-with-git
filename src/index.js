6722107dc67edd101cedab160db400084a05c1c8
vuhi <vuhi@mail.uc.edu>
2021-01-15 17:20:22 -0500

    use aws sdk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
-const axios = require('axios');
+// const axios = require('axios');
+const aws = require('aws-sdk');
+
 
 app.get('/', (req, res) => {
-  // TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-  const result = axios.get(
-    // 'http://169.254.169.254/latest/api/token',
-      'http://169.254.169.254/latest/meta-data/instance-id',
-      {},
-      // { headers: { 'X-aws-ec2-metadata-token-ttl-seconds': 21600 }}
-  )
-  .then(result => res.json({ result }))
-  .catch(error => res.json({ error }));
+  // const result = axios.get(
+  //   // 'http://169.254.169.254/latest/api/token',
+  //   //   'http://169.254.169.254/latest/meta-data/instance-id',
+  //     {},
+  //     { headers: { 'X-aws-ec2-metadata-token-ttl-seconds': 21600 }}
+  // )
+  // .then(result => res.json({ result }))
+  // .catch(error => res.json({ error }));
+
+  new aws.MetadataService().request('latest/meta-data/instance-id', (err, data) => {
+    res.json({ err, data });
+  })
 });
 
 const server = app.listen(port, () => {
