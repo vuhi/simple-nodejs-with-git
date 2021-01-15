e4178aadd767587c063308f198dad34bf4d91c9a
vuhi <vuhi@mail.uc.edu>
2021-01-15 17:33:28 -0500

    try something out

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8081;
 const aws = require('aws-sdk');
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   // const result = axios.get(
   //   // 'http://169.254.169.254/latest/api/token',
   //   //   'http://169.254.169.254/latest/meta-data/instance-id',
@@ -15,9 +15,13 @@ app.get('/', (req, res) => {
   // .then(result => res.json({ result }))
   // .catch(error => res.json({ error }));
 
-  new aws.MetadataService().request('/latest/meta-data/', (err, data) => {
-    res.json({ err, data });
-  });
+  // new aws.MetadataService().request('/latest/meta-data/', (err, data) => {
+  //   res.json({ err, data });
+  // });
+
+
+  const result = await new aws.MetadataService().request('/latest/meta-data/');
+  res.json({ result });
 });
 
 const server = app.listen(port, () => {
