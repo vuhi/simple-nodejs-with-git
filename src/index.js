5697e2f4e19e1d913d2f6f27f26e36c11c6e3bfd
vuhi <vuhi@mail.uc.edu>
2021-01-15 16:36:32 -0500

    add header

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = process.env.PORT || 8081;
 const axios = require('axios');
 
-app.get('/', async (req, res) => {
-  
-  try {
-    const result = await axios.get('http://169.254.169.254/latest/meta-data/');
-    res.json({ result });
-  } catch(err) {
-    res.json({ err });
-  }
+app.get('/', (req, res) => {
+  // TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+  const result = axios.get(
+    'http://169.254.169.254/latest/api/token',
+    { headers: { 'X-aws-ec2-metadata-token-ttl-seconds': 21600 }}
+  )
+  .then(result => res.json({ result }))
+  .catch(error => res.json({ error }));
 });
 
 const server = app.listen(port, () => {
