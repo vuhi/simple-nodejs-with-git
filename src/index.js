511ae2cb33cb4835c86e5f540fdbc55b2e0e80f0
vuhi <vuhi@mail.uc.edu>
2021-01-15 18:28:46 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
 const { getMetaDataItems } = require('./metadata.service');
-
+const aws = require('aws-sdk');
 
 
 app.get('/', async (req, res) => {
   try {
-    const items = await getMetaDataItems();
+    // const items = await getMetaDataItems();
     // const ec2Metadata = await getMetaData(items);
-    res.json({ items });
+
+
+    new aws.MetadataService.request('/latest/meta-data/', (err, data) => {
+      if (err) {
+        res.json({ err });
+      }
+
+      const metadata = data.split(/\r\r|\r|\n/);
+      res.json({ metadata });
+    });
+
+    // res.json({ items });
   } catch (error) {
     res.json({ error });
   }
