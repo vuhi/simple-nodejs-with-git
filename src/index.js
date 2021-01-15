62e2c10f437553d867bc41d2cebef75c3d7840ef
vuhi <vuhi@mail.uc.edu>
2021-01-15 18:16:25 -0500

    try to get all meta-data again

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
-// const axios = require('axios');
-const aws = require('aws-sdk');
+const { getMetaDataItems, getMetaData } = require('metadata.service');
 
 
-app.get('/', (req, res) => {
-  const metadataService = new aws.MetadataService();
 
-  metadataService.request('/latest/meta-data/', (err, data) => {
-    if (err) {
-      res.json({ err });
-      return;
-    }
-    const metadata = data.split(/\r\r|\r|\n/);
-    const ec2Metadata = {};
-
-    metadata.forEach(item => {
-      metadataService.request(`/latest/meta-data/${item}`, (err, data) => {
-        if (err) {
-          res.json({ err });
-          return;
-        }
-        ec2Metadata[item] = data;
-      })
-    });
-
+app.get('/', async (req, res) => {
+  try {
+    const items = await getMetaDataItems();
+    const ec2Metadata = await getMetaData(items);
     res.json({ ec2Metadata });
-  });
-
-
-
-
-
-
+  } catch (error) {
+    res.json({ error });
+  }
 });
 
 const server = app.listen(port, () => {
