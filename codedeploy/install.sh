0576a22d412d0d2028c1fa350d26355e00594061
vuhi <vuhi@mail.uc.edu>
2021-01-22 17:17:18 -0500

    remove build tools

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -2,22 +2,18 @@
 set -e
 
 # update instance
-echo "update instance"
 yum -y update
 
 # add nodejs to yum
-echo "add nodejs to yum"
 curl -sL https://rpm.nodesource.com/setup_12.x | bash -
 
 # install nodejs
-echo "install nodejs"
 yum -y install nodejs
 
 # build tools
-echo "build tools"
-yum install gcc-c++ make
+# echo "build tools"
+# yum install gcc-c++ make
 
 # install pm2 module globaly
-echo "install pm2 module globaly"
 sudo npm install pm2@latest -g
 pm2 update
